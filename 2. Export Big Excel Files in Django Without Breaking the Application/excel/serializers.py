2ca10561937ba928953167228814e0b490810507
Francisco Arocas <54645175+franciscoarocas@users.noreply.github.com>
2026-07-10 19:28:51 +0100

    Update serializers.py

--- 2. Export Big Excel Files in Django Without Breaking the Application/excel/serializers.py
+++ 2. Export Big Excel Files in Django Without Breaking the Application/excel/serializers.py
@@ -1,20 +1,21 @@
-from rest_framework import serializers
 from django.utils.text import get_valid_filename
+from rest_framework import serializers
 
-from excel.constants import DEFAULT_EXPORT_FILENAME
+from excel.constants import DEFAULT_EXPORT_FILENAME, EXPORT_COLUMNS
+from excel.models import Example
 
 
-class ExampleExportSerializer(serializers.Serializer):
-    col_a = serializers.CharField(max_length=255)
-    col_b = serializers.CharField(max_length=255)
-    col_c = serializers.CharField(max_length=255)
-    col_d = serializers.CharField(max_length=255)
+class ExampleExportSerializer(serializers.ModelSerializer):
     filename = serializers.CharField(
         max_length=100,
         required=False,
         allow_blank=True,
         default=DEFAULT_EXPORT_FILENAME,
     )
+
+    class Meta:
+        model = Example
+        fields = (*EXPORT_COLUMNS, 'filename')
 
     def validate_filename(self, filename):
         filename = get_valid_filename(filename or DEFAULT_EXPORT_FILENAME)
