dc46914b83a9ecf31771ef390602397b01072f51
Francisco Arocas <54645175+franciscoarocas@users.noreply.github.com>
2026-07-10 19:26:34 +0100

    Create constants.py

---
+++ 2. Export Big Excel Files in Django Without Breaking the Application/excel/constants.py
@@ -0,0 +1,5 @@
+DEFAULT_EXPORT_FILENAME = 'example'
+EXCEL_CONTENT_TYPE = (
+    'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+)
+EXPORT_COLUMNS = ('col_a', 'col_b', 'col_c', 'col_d')
